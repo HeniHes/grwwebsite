7c5af2ce20c470068b913bb8ec4dce865bd2ab76
HeniHes <heni.es.paypal@gmail.com>
2025-05-04 23:22:03 +0100

    Add Vercel config for nested directory

--- grwwebsite/pages/_app.js
+++ grwwebsite/pages/_app.js
@@ -1,11 +1,19 @@
 // pages/_app.js
-import '../styles/globals.css'
-import { LocaleProvider } from '../contexts/LocaleContext'
+import "../styles/globals.css";
+import { LocaleProvider } from "../contexts/LocaleContext";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <LocaleProvider>
+      <Head>
+        {/* Using the existing image files from public/images directory */}
+        <link rel="icon" href="/images/iconlogo.jpg" />
+        <link rel="icon" type="image/jpeg" href="/images/iconlogo.jpg" />
+        <link rel="apple-touch-icon" href="/images/iconlogo.jpg" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Component {...pageProps} />
     </LocaleProvider>
-  )
+  );
 }
